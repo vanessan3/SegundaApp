7494c287b93d5712bff26d36e03965975cfc98db
Vanessa <vviana1512@gmail.com>
2018-08-14 22:07:56 -0300

    teste

---
+++ app/src/main/java/br/com/etecia/segundaapp/CadastroActivity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.segundaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro_layout);
+    }
+}
